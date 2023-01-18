3a95705fde1c446a9c6637c34a41cfb2c4e34fb6
imjoseangel <josea.munoz@gmail.com>
2023-01-18 20:04:08 +0100

    add log anal and insights

--- main.tf
+++ main.tf
@@ -21,3 +21,31 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = merge({ "ResourceName" = format("%s", var.resource_group_name) }, var.tags, )
 }
+
+#---------------------------------------------------------
+# Log Analytics Creation or selection
+#---------------------------------------------------------
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = var.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 90
+  daily_quota_gb      = 5
+  tags                = merge({ "ResourceName" = lower(var.name) }, var.tags, )
+}
+
+#---------------------------------------------------------
+# Insights Creation or selection
+#---------------------------------------------------------
+
+resource "azurerm_application_insights" "main" {
+  name                = var.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.main.id
+
+  tags = merge({ "ResourceName" = lower(var.name) }, var.tags, )
+}
